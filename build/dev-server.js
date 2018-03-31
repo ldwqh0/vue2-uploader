79999b5998b88ca5dd701d5be818a64fb07b4af2
ldwqh0 <ldwqh0@outlook.com>
2018-03-31 11:39:34 +0800

    版本0.1.0

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,7 @@
-const webpack=require('webpack')
-const WebpackDevServer=require('webpack-dev-server')
-const config=require('./webpack.dev.conf')
-config.then(conf=>{
-    const server=new WebpackDevServer(webpack(conf),conf.devServer)
-    server.listen(conf.devServer.port)
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.dev.conf')
+config.then(conf => {
+  const server = new WebpackDevServer(webpack(conf), conf.devServer)
+  server.listen(conf.devServer.port)
 })
